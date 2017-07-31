bc0c574caf67c949c082b7565abb55c5acf77a1e
Tim Graham <tim.graham1982@gmail.com>
2017-07-31 21:02:53 +0100

    1st version with initial countdown starting with a start button.

--- src/main.py
+++ src/main.py
@@ -1,23 +1,63 @@
 '''
-Created on 16 Feb 2017
+Created on 30 Jul 2017
 
 @author: tim
 '''
+
 from kivy.app import App
-from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.properties import NumericProperty
+from kivy.clock import Clock
 
 
-class MyClass(App):
+class HomeScreen(BoxLayout):
     '''
-    classdocs
+    Main page containing the buttons and timers.
     '''
-    def build(self):
-        return Button(text='Hello!',
-                      background_color=(0, 0, 1, 1),  # List of
-                                                      # rgba components
-                      font_size=150)
-    
-    
-if __name__=="__main__":
-    MyClass().run()
+    def __init__(self, **kwargs):
+        super(HomeScreen, self).__init__(**kwargs)
+        self.ids.clock1.seconds_to_start = 300
         
+    def start(self):
+        self.event = Clock.schedule_interval(self.change_time, 1)
+    
+    def change_time(self, dt):
+        self.ids.clock1.seconds_to_start -= dt
+
+
+class ClockDisplay(Label):
+    scale_factor = .9
+    factor = dimension = None
+    seconds_to_start = NumericProperty(180)
+    
+    def on_texture_size(self, *args):
+        try:
+            if not self.factor:
+                self.factor = [self.font_size / self.texture_size[0], 
+                               self.font_size / self.texture_size[1]]
+    
+            self.font_size0 = self.size[0] * self.scale_factor * self.factor[0]
+            self.font_size1 = self.size[1] * self.scale_factor * self.factor[1]
+    
+            if self.font_size0 < self.font_size1:
+                self.font_size = self.font_size0
+            else:
+                self.font_size = self.font_size1
+        except ZeroDivisionError:
+            pass
+    
+    def text_formatter(self, *args):
+        mins = self.seconds_to_start/60
+        secs = self.seconds_to_start % 60
+        
+        self.text = '%d' % mins + ':' '%02d' % secs
+
+
+class TimerApp(App):
+    def build(self):
+        return HomeScreen()
+        
+
+if __name__ == '__main__':
+    TimerApp().run()
