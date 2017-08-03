f2d2a2cd46ff46e3702324dd0a673f77b014952e
Tim Graham <tim.graham1982@gmail.com>
2017-08-03 19:09:25 +0100

    Working timer for one race only with settings panel

---
+++ src/settings.py
@@ -0,0 +1,23 @@
+import json
+
+settings_json = json.dumps([
+    {'type': 'options',
+     'title': 'Start Sequence',
+     'desc':'Choose the type of start sequence',
+     'options': ['5, 4, 1, Go', '6, 3, 1, Go', '6, 3, Go', '3, 2, 1, Go'],
+     'section': 'Race settings',
+     'key': 'sequence'
+    },
+    {'type': 'numeric',
+     'title': 'Number of starts',
+     'desc': 'Number of back to back starts in sequence (max 4)',
+     'section': 'Race settings',
+     'key': 'nstarts'
+     },
+    {'type': 'numeric',
+     'title': 'Time between starts (minutes)',
+     'desc': 'Time between starts in minutes',
+     'section': 'Race settings',
+     'key' : 'interval'
+     }
+    ])
